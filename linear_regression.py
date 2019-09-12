19cba9b1558a817d915f582a72eb839855a24ca6
kirmanid <kirmanid@mssm.org>
2019-09-12 19:15:32 -0400

    Now it works on the seaborn dataset :D

--- linear_regression.py
+++ linear_regression.py
@@ -1,41 +1,64 @@
-w = [1,1]
-x = [[1,5],[1,4],[1,3],[1,2],[1,1]]
-y = [2,5,6,8,10]
+import numpy as np
+import seaborn as sns
 
-learningRate = 0.1
+class LinearRegression:
+    def __init__(self,x):
+        self.w = np.array([1]*(np.size(x[0])+1)) # this sets the number of weights to the number of features +1
 
-def hypothesis(w,xRow):
-    # make dot product later
-    return w[0]*xRow[0] + w[1]*xRow[1]
+    def hypothesis(self, xRow):
+        return np.dot (self.w, xRow)
 
-def getCost(w,x,y):
-    m = len(y)
-    z = 0
-    for i in range (m):
-        z += (hypothesis(w,x[i])-y[i])**2
-    z = z/(2*m)
-    return z
-
-def takeCostDeriv(w,x,y):
-    m = len(y)
-    d = [0]*len(w)
-    for weightIndex in range (len(w)):
+    def getCost(self,x,y):
+        m = len(y)
         z = 0
         for i in range (m):
-            z += (hypothesis(w,x[i])-y[i])*x[i][weightIndex]
-        z = z/m
-        d[weightIndex] = z
-    return d
+            z += (self.hypothesis(x[i])-y[i])**2
+        z = z/(2*m)
+        return z
 
-def doGradientDescent(learningRate, w, x, y):
-    d = takeCostDeriv(w,x,y)
-    newWeights = [0] * len(w)
-    for i in range (len(w)):
-        newWeights[i] = w[i] - (learningRate*d[i])
-    return newWeights
+    def takeCostDeriv(self,x,y):
+        m = len(y)
+        d = [0]*len(self.w)
+        for weightIndex in range (len(self.w)):
+            z = 0
+            for i in range (m):
+                z += (self.hypothesis(x[i])-y[i])*x[i][weightIndex]
+            z = z/m
+            d[weightIndex] = z
+        return d
 
-for i in range (200):
-    w = doGradientDescent(learningRate, w, x, y)
-    print (w)
+    def doGradientDescent(self, learningRate, x, y):
+        d = self.takeCostDeriv(x,y)
+        newWeights = np.zeros(len(self.w))
+        for i in range (len(self.w)):
+            newWeights[i] = self.w[i] - (learningRate*d[i])
+        return newWeights
 
-print (getCost(w,x,y))
+    def addXNoughts(self, x):
+        x = np.reshape(x,((x.shape[0],np.size(x[0])))) # reshapes flat array or 2d array into transposed version
+        x0 = np.ones((x.shape[0],1), dtype=np.uint8)
+        x = np.hstack((x0,x))
+        return x
+    
+    def train(self, epochs, x, y, learningRate):
+        x = self.addXNoughts(x)
+        for i in range (epochs):
+            self.w = self.doGradientDescent(learningRate, x, y)
+            print ("epoch " + str(i))
+            #print(self.getCost(x,y)) # shows progress
+        print("\nFinal Cost:")
+        print (self.getCost(x,y))
+
+learningRate = 0.01
+epochs = 200
+
+iris = sns.load_dataset('iris').to_numpy()[:,:4].astype(np.float32)
+np.random.shuffle(iris)
+
+x = iris[:, 0:3]
+y = iris[:, 3:4]
+
+
+a = LinearRegression(x)
+a.train(epochs,x,y,learningRate)
+
